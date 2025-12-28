750b525957882de0faed76e60452739dad53d443
Your Name <youremail@example.com>
2025-12-28 15:03:15 +0530

    updated links

--- src/data.js
+++ src/data.js
@@ -116,8 +116,8 @@ export const projects = [
     duration: "4 weeks",
     description:
       "Responsive React e‑commerce UI where users browse products, manage cart items and complete a mock checkout flow.",
-    liveUrl: "",
-    codeUrl: ""
+    liveUrl: "busybuy25.netlify.app",
+    codeUrl: "https://github.com/Kadyan25/Busy-Buy"
   },
   {
     name: "AI‑Enabled B2B Fintech App",
